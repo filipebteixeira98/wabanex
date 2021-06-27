52665be7d8da819cecfd71250501db5ea6ba9a30
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-27 19:14:18 -0300

    feat: create user type to root schema

---
+++ lib/wabanex_web/schema/types/user.ex
@@ -0,0 +1,10 @@
+defmodule WabanexWeb.Schema.Types.User do
+  use Absinthe.Schema.Notation
+
+  @desc "user representation"
+  object :user do
+    field :id, non_null(:uuid4)
+    field :name, non_null(:string)
+    field :email, non_null(:string)
+  end
+end

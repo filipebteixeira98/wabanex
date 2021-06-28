37e067756ff83f12c63842f17552a9315ad7e91b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-06-27 23:53:32 -0300

    feat: create exercises schema

--- lib/wabanex/training.ex
+++ lib/wabanex/training.ex
@@ -13,7 +13,7 @@ defmodule Wabanex.Training do
     field :start_date, :date
     field :end_date, :date
 
-    belongs_to :user, User
+    belongs_to :users, User
     has_many :exercises, Exercise
 
     timestamps()
